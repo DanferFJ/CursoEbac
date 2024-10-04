23ac496d3eb6ef00a9d4285555d8a279af169809
DanferFJ <Daniel.fernandes.tr@gmail.com>
2024-10-03 23:52:05 -0300

    Adicionado botão de saída
    Botão de saida

--- CartoriodaEBAC.c
+++ CartoriodaEBAC.c
@@ -125,7 +125,8 @@ int main()
 		printf("Escolha a opção desejada do menu:\n\n");
 		printf("\t1 - Registrar nomes\n");
 		printf("\t2 - Consultar nomes\n");
-		printf("\t3 - Deletar nomes\n\n"); //Fim do menu
+		printf("\t3 - Deletar nomes\n"); 
+		printf("\t4 - Fechar programa\n\n");//Fim do menu
 		printf("Qual opção desejada? ");
 		scanf("%d", &opcao); //Aramazenando as informações do usuario
 		
@@ -143,6 +144,11 @@ int main()
 				
 			case 3:
 			deletar();
+			break;
+			
+			case 4:
+			printf("sistema sendo finalizado!\n");
+			return 0;
 			break;
 		
 			default:
